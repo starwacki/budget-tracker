58078e89f5ff36c7d6823888b582687498c132de
Szymon Tarwacki <tarwackiszymon7@gmail.com>
2023-07-18 14:38:16 +0200

    Init commit

---
+++ src/test/java/com/starwacki/budgettracker/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwacki.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
